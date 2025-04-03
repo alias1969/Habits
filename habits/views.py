2ce740a7931c1983c98816db0a4878ce616d33cb
Irina Kamneva <brynza80@gmail.com>
2025-04-03 19:17:48 +0300

    initial project

---
+++ habits/views.py
@@ -0,0 +1,73 @@
+from rest_framework.generics import CreateAPIView, UpdateAPIView, DestroyAPIView, RetrieveAPIView, ListAPIView
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.serializers import ValidationError
+
+from habits.models import Habit
+from habits.paginations import HabitPaginator
+from habits.permissions import IsOwner
+from habits.serializers import HabitSerializer
+from habits.services import telegram_message
+
+
+class HabitCreateAPIView(CreateAPIView):
+    """Создание привычки"""
+    serializer_class = HabitSerializer
+    queryset = Habit.objects.all()
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        """Отправить сообщение пользователю в Телеграм о создании привычки"""
+        habit = serializer.save()
+        habit.user = self.request.user
+        habit = serializer.save()
+        habit.save()
+        if habit.user.tg_id:
+            telegram_message(habit.user.tg_id, 'Создана новая привычка!')
+
+
+class HabitUpdateAPIView(UpdateAPIView):
+    """Изменение привычки"""
+    serializer_class = HabitSerializer
+    queryset = Habit.objects.all()
+    permission_classes = [IsAuthenticated, IsOwner]
+
+    def perform_update(self, serializer):
+        """Проверка связанной привычки на саму себя """
+        habit = serializer.save()
+        if not habit.related_habit and habit.id == habit.related_habit.id:
+            raise ValidationError(
+                f"Связанная привычка не может быть ссылкой на саму себя!"
+            )
+        habit.save()
+
+
+class HabitDeleteAPIView(DestroyAPIView):
+    """Удаление привычки"""
+    queryset = Habit.objects.all()
+    permission_classes = [IsAuthenticated, IsOwner]
+
+
+class HabitRetrieveAPIView(RetrieveAPIView):
+    """Просмотр привычки"""
+    serializer_class = HabitSerializer
+    queryset = Habit.objects.all()
+    permission_classes = [IsAuthenticated, IsOwner]
+
+
+class HabitListAPIView(ListAPIView):
+    """Список всех привычек"""
+    serializer_class = HabitSerializer
+    queryset = Habit.objects.all()
+    permission_classes = [IsAuthenticated, IsOwner]
+
+    def get_queryset(self):
+        return Habit.objects.filter(user=self.request.user)
+
+
+class HabitPublicListAPIView(ListAPIView):
+    """Список опубликованных привычек"""
+    serializer_class = HabitSerializer
+    queryset = Habit.objects.filter(is_public=True)
+    permission_classes = [AllowAny]
+    pagination_class = HabitPaginator
+
---
+++ user/views.py
@@ -0,0 +1,57 @@
+from rest_framework.generics import CreateAPIView, UpdateAPIView, DestroyAPIView, ListAPIView, RetrieveAPIView
+
+from user.models import User
+from user.serializer import UserSerializer, UserTokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework.permissions import AllowAny, IsAuthenticated
+
+
+class UserCreateAPIView(CreateAPIView):
+    """Регистрация нового пользователя"""
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    permission_classes = [AllowAny]
+
+    def perform_create(self, serializer):
+        """Активация учетной записи пользователя"""
+        user = serializer.save(is_active=True)
+        user.set_password(self.request.data.get('password'))
+        user.save()
+
+
+class UserUpdateAPIView(UpdateAPIView):
+    """Изменение профиля пользователя"""
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    permission_classes = [IsAuthenticated]
+
+    def password_update(self, serializer):
+        """Обновление и сохранение пароля при изменении"""
+        user = serializer.save()
+        user.set_password(self.request.data.get('password'))
+        user.save()
+
+
+class UserDeleteAPIView(DestroyAPIView):
+    """Удаление пользователя"""
+    queryset = User.objects.all()
+    permission_classes = [IsAuthenticated]
+
+
+class UserListAPIView(ListAPIView):
+    """Список профилей"""
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    permission_classes = [IsAuthenticated]
+
+
+class UserRetrieveAPIView(RetrieveAPIView):
+    """Карточка пользователя"""
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    permission_classes = [IsAuthenticated]
+
+
+class UserTokenObtainPairView(TokenObtainPairView):
+    """ Получение токена """
+    serializer_class = UserTokenObtainPairSerializer
